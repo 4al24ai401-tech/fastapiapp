fb6cfc452ef8bc3c191c925e479d21e73f8b9799
supreeth <4al24ai401@aiet.org.in>
2026-06-27 14:07:34 +0530

    test4

--- app/main.py
+++ app/main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
+from database import engine, Base
 from routers import company,job
-app=FastAPI()
+
+app = FastAPI()
+print("engine is :", engine)
+Base.metadata.create_all(bind=engine)
 app.include_router(company.router)
 app.include_router(job.router)
 
+
 @app.get("/")
 def read_root():
-    return{ "Hello"  : "World"}
-
+    return {"Hello": "World"}
 
 @app.get("/about")
 def read_about():
     return {"About": "This is about page."}
-
-
 
 @app.get("/contact")
 def read_contact():
--- schemas/company.py
+++ schemas/company.py
@@ -1,11 +1,27 @@
+
 from pydantic import BaseModel
 from typing import Optional
+from .job import JobResponse,JobBase
 
-class companyCreate(BaseModel):
+class companyBase(BaseModel):
     name: str
-    location: str
-    
+    email: str
+    phone: str
 
-class companyUpdate(BaseModel):
+class companyCreate(companyBase):
+    pass
+
+
+
+class companyUpdate(companyBase):
     name: Optional[str] = None
-    location: Optional[str] = None
+    email: Optional[str] = None
+    phone: Optional[str] = None
+
+class companyResponse(companyBase):
+    id: int
+    jobs: list[JobResponse]
+
+    class Config:
+        from_attributes = True
+
