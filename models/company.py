675907c85ec68e844d26a04beabac97f0be071b1
supreeth <4al24ai401@aiet.org.in>
2026-06-29 11:16:42 +0530

    alembic

--- models/company.py
+++ models/company.py
@@ -13,4 +13,5 @@ class Company(Base):
     name = Column(String, nullable=False, index=True)
     email = Column(String, unique=True)
     phone = Column(String, index=True)
+    location=column(string)
     jobs = relationship("Job", back_populates="company")
