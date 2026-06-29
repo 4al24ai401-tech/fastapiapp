a911eb3a6b368b5b9ee6feb7335fd9abbe2a13a0
supreeth <4al24ai401@aiet.org.in>
2026-06-29 12:27:57 +0530

    txt1

--- database.py
+++ database.py
@@ -1,18 +1,22 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456789@localhost:5432/student_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Harsh%40020@localhost:5432/student_db"
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
 
 
 def get_db():
-
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
--- models/job.py
+++ models/job.py
@@ -1,19 +1,22 @@
-from sqlalchemy import Column, Integer, String, Enum,ForeignKey
-from models.company import Company
-from sqlalchemy.orm import declarative_base,relationship
-from database import Base , engine ,SessionLocal
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
 
-
-Base = declarative_base()
 
 class Job(Base):
     __tablename__ = "jobs"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False, )
-    description = Column(String, nullable=False)
-    salary = Column(Integer, nullable=False)
-    company_id = Column(Integer, ForeignKey('companies.id'), nullable=False)
-    company = relationship("Company", back_populates="jobs")
+    title = Column(String, nullable=False)
+    description = Column(String)
+    salary = Column(Integer)
 
+    company_id = Column(
+        Integer,
+        ForeignKey("companies.id")
+    )
 
+    company = relationship(
+        "Company",
+        back_populates="jobs"
+    )
